c84284df7e83f816be607c5510e1158f3894f466
Kaushal-Kishore-Gupta <thekaushalkishoregupta@gmail.com>
2023-01-27 15:58:47 +0530

    Joined Authentiaction

--- myApp/urls.py
+++ myApp/urls.py
@@ -4,5 +4,8 @@ from myApp import views
 
 urlpatterns = [
     path("", views.create_short_url, name="create_short_url"),
+    path("login", views.loginuser, name="login"),
+    path("logout", views.logoutuser, name="logout"),
+    path("signup",views.signupuser,name="signup"),
     path('<str:shortenedd>', views.redirect_url, name='redirect_url'),
 ]
--- myApp/views.py
+++ myApp/views.py
@@ -3,9 +3,14 @@ from myApp.models import URL
 import random
 import string
 from django.views.generic.base import RedirectView
+from django.contrib.auth import login, logout, authenticate
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
 # Create your views here.
 
 def create_short_url(request):
+    if not request.user.is_authenticated:
+        return redirect('login')
     shortened_url = None
     if request.method == "POST":
         original_url = request.POST.get("original_url")
@@ -18,3 +23,44 @@ def create_short_url(request):
 def redirect_url(request, shortenedd):
     url = URL.objects.get(shortened_url=shortenedd)
     return RedirectView.as_view(url=url.original_url, permanent=True)(request)
+
+def loginuser(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+        print(username,password)
+        if user is not None:
+            login(request, user)
+            return redirect('/')
+        else:
+            return render(request, 'login.html', {'error_message': 'Invalid login'})
+    else:
+        return render(request, 'login.html')
+
+def logoutuser(request):
+    if not request.user.is_authenticated:
+        return redirect('login')
+    logout(request)
+    return render(request, "logout.html")   
+
+def signupuser(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        email = request.POST.get('email')
+        fname = request.POST.get('fname')
+        lname = request.POST.get('lname')
+        if User.objects.filter(username=username).exists():
+            return render(request, 'signup.html', {'error_message': 'Username already exists'})
+        if User.objects.filter(email=email).exists():
+            return render(request, 'signup.html', {'error_message': 'Email already exists'})
+        user = User.objects.create_user(username, email, password)
+        user.first_name = fname
+        user.last_name = lname
+        user.save()
+        print(username,password,email)
+        return redirect('login')
+    return render(request, "signup.html")
+
+
