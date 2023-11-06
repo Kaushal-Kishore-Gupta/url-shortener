9d8bf251ef659d1713294058bfcad307544c0a07
Kaushal-Kishore-Gupta <thekaushalkishoregupta@gmail.com>
2023-11-06 22:34:20 +0530

    backup

--- urlshortener/settings.py
+++ urlshortener/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-7#kt7%#wt#@sg(irg8^8v41jk8ioary+)l*pj77-1t65wl-92p
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
